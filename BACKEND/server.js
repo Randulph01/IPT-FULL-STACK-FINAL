f65fd0b82cc11e62931a3b0332cf95b8a2fdd067
John Michael Vincent Bajenting <bajentingjohn@gmail.com>
2025-05-17 17:31:34 +0800

    Updated Backend

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -18,7 +18,7 @@ app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: 
 // api routes
 app.use('/accounts', require('./accounts/accounts.controller'));
 app.use('/employees', require('./employees/employees.controller'));
-app.use('/departments', require('./departments/departments.controller'));
+app.use('/departments', require('./departments/department.controller'));
 app.use('/workflows', require('./workflows/workflows.controller'));
 app.use('/requests', require('./requests/request.controller'));
 
